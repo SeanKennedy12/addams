5ab080bfae083c3129cd8ea01e0ac8a02ea63fcd
Sean Kennedy Desktop <seank462@gmail.com>
2024-05-03 21:03:51 +0100

    eventbrite trial

--- app/buy-tickets/page.js
+++ app/buy-tickets/page.js
@@ -1,8 +1,43 @@
+'use client';
+
 import React from 'react';
 import Image from "next/image";
 import Link from 'next/link';
+import { useEffect } from 'react';
 
 export default function BuyTickets() {
+
+    useEffect(() => {
+        // Function to create Eventbrite widget
+        const createWidget = () => {
+            var exampleCallback = function () {
+                console.log('Order complete!');
+            };
+
+            if (typeof window !== 'undefined' && window.EBWidgets) {
+                window.EBWidgets.createWidget({
+                    // Required
+                    widgetType: 'checkout',
+                    eventId: '887221572607',
+                    iframeContainerId: 'eventbrite-widget-container-887221572607',
+
+                    // Optional
+                    iframeContainerHeight: 425,
+                    onOrderComplete: exampleCallback
+                });
+            }
+        };
+
+        createWidget();
+
+        // Clean-up function to remove the widget when component unmounts
+        return () => {
+            // If needed, remove the widget here
+        };
+    }, []);
+
+
+
     return (
         <>
             <main className='px-8 py-6'>
