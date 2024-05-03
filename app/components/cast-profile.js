97028ceab5dd93f1556c55887a345abe06dc18cc
Sean Kennedy Desktop <seank462@gmail.com>
2024-05-03 20:55:34 +0100

    priority added to images

--- app/components/cast-profile.js
+++ app/components/cast-profile.js
@@ -27,8 +27,8 @@ export default function CastProfile({ img, img2, character, actor, alt }) {
             <Head>
                 <link rel="preload" as="image" href={img2} />
             </Head>
-            <Image width={200} height={200} className='rounded-full md:ml-auto md:mr-8' onMouseOver={handleMouseOver} onMouseOut={handleMouseOut} onClick={handleClick} src={image} alt={alt} />
-            <Image width={200} height={200} className='rounded-full md:ml-auto md:mr-8 hidden' src={img2} />
+            <Image width={200} height={200} className='rounded-full md:ml-auto md:mr-8' onMouseOver={handleMouseOver} onMouseOut={handleMouseOut} onClick={handleClick} src={image} alt={alt} priority={true} />
+            <Image width={200} height={200} className='rounded-full md:ml-auto md:mr-8 hidden' src={img2} priority={true} />
             <div className='ml-0 flex flex-col md:justify-start items-center md:items-start md:mr-auto'>
                 <h3 className='text-2xl md:text-3xl font-semibold mt-4 md:mt-0'>{character}</h3>
                 <p className='text-xl mt-2'>{actor}</p>
