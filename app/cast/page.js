34a85431614f570b9d3cf9476955702cd5621337
Sean Kennedy Desktop <seank462@gmail.com>
2024-05-03 20:18:45 +0100

    another try

--- app/cast/page.js
+++ app/cast/page.js
@@ -25,6 +25,21 @@ export default function Cast() {
                         <CastProfile img='/static/images/portraits/MorticiaB_1.jpg' img2='/static/images/portraits/MorticiaB_2.jpg' character='Morticia Addams' actor='Chloe Miller' alt='Morticia Adams' />
                         <CastProfile img='/static/images/portraits/WednesdayB_1.jpg' img2='/static/images/portraits/WednesdayB_2.jpg' character='Wednesday Addams' actor='Alecia Dalziel' alt='Wednesday Adams' />
                     </section>
+                    {/* Other Cast */}
+                    <section className='grid md:grid-cols-3 gap-y-8 md:gap-y-12 justify-center md:justify-between mt-16'>
+                        <CastProfile img='/static/images/portraits-compressed/Gomez_1.jpg' img2='/static/images/portraits-compressed/Gomez_2.jpg' character='Gomez Addams' actor='Scott Taylor' alt='Gomez Adams' />
+                        <CastProfile img='/static/images/portraits/MorticiaA_1.jpg' img2='/static/images/portraits/MorticiaA_2.jpg' character='Morticia Addams' actor='Erin Cameron' alt='Morticia Adams' />
+                        <CastProfile img='/static/images/portraits/WednesdayA_1.jpg' img2='/static/images/portraits/WednesdayA_2.jpg' character='Wednesday Addams' actor='Paige Brogan' alt='Wednesday Adams' />
+                        <CastProfile img='/static/images/portraits/Pugsley_1.jpg' img2='/static/images/portraits/Pugsley_2.jpg' character='Pugsley Addams' actor='Kyle Chaplin' alt='Pugsley Adams' />
+                        <CastProfile img='/static/images/portraits/Uncle_1.jpg' img2='/static/images/portraits/Uncle_2.jpg' character='Uncle Fester' actor='Jamie Prentice' alt='Uncle Fester' />
+                        <CastProfile img='/static/images/portraits/Lurch_1.jpg' img2='/static/images/portraits/Lurch_2.jpg' character='Lurch' actor='Alasdair McLean' alt='Lurch' />
+                        <CastProfile img='/static/images/portraits/GrandmaA_1.jpg' img2='/static/images/portraits/GrandmaA_2.jpg' character='Grandma' actor='Jessica O&apos;Toole' alt='Grandma' />
+                        <CastProfile img='/static/images/portraits/Lucas_1.jpg' img2='/static/images/portraits/Lucas_2.jpg' character='Lucas' actor='Jack Harris' alt='Lucas' />
+                        <CastProfile img='/static/images/portraits/D_1.jpg' img2='/static/images/portraits/D_2.jpg' character='Mal' actor='Adam McLean' alt='Mal' />
+                        <CastProfile img='/static/images/portraits-compressed/Alice_1.jpg' img2='/static/images/portraits-compressed/Alice_2.jpg' character='Alice' actor='Eden Hardie' alt='Alice' />
+                        <CastProfile img='/static/images/portraits/MorticiaB_1.jpg' img2='/static/images/portraits/MorticiaB_2.jpg' character='Morticia Addams' actor='Chloe Miller' alt='Morticia Adams' />
+                        <CastProfile img='/static/images/portraits/WednesdayB_1.jpg' img2='/static/images/portraits/WednesdayB_2.jpg' character='Wednesday Addams' actor='Alecia Dalziel' alt='Wednesday Adams' />
+                    </section>
                 </div>
 
                 <div className='flex justify-center lg:mt-4'>
--- app/components/cast-profile.js
+++ app/components/cast-profile.js
@@ -28,6 +28,7 @@ export default function CastProfile({ img, img2, character, actor, alt }) {
                 <link rel="preload" as="image" href={img2} />
             </Head>
             <Image width={200} height={200} className='rounded-full md:ml-auto md:mr-8' onMouseOver={handleMouseOver} onMouseOut={handleMouseOut} onClick={handleClick} src={image} alt={alt} />
+            <Image width={200} height={200} className='rounded-full md:ml-auto md:mr-8 hidden' src={img2} />
             <div className='ml-0 flex flex-col md:justify-start items-center md:items-start md:mr-auto'>
                 <h3 className='text-2xl md:text-3xl font-semibold mt-4 md:mt-0'>{character}</h3>
                 <p className='text-xl mt-2'>{actor}</p>
