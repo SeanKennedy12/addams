cad643c3c665427ba6bb03b18969b686e797f6db
Sean Kennedy Desktop <seank462@gmail.com>
2024-05-03 20:48:24 +0100

    other page templates added

---
+++ app/buy-tickets/page.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Image from "next/image";
+import Link from 'next/link';
+
+export default function BuyTickets() {
+    return (
+        <>
+            <main className='px-8 py-6'>
+                <h1 className='text-4xl font-semibold pb-6 text-center'>Buy Tickets</h1>
+
+            </main>
+
+        </>
+    )
+}
---
+++ app/contact/page.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Image from "next/image";
+import Link from 'next/link';
+
+export default function Contact() {
+    return (
+        <>
+            <main className='px-8 py-6'>
+                <h1 className='text-4xl font-semibold pb-6 text-center'>Contact Us</h1>
+
+            </main>
+
+        </>
+    )
+}
